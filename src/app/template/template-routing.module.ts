ec65985e9c9a9e98ba9316f1fe2f6285299bf1c7
jeffconexion <jeff.debian2016@gmail.com>
2026-01-28 18:14:20 -0300

    feat: enhance layout component with dynamic title and subtitle from route data

--- src/app/template/template-routing.module.ts
+++ src/app/template/template-routing.module.ts
@@ -10,17 +10,29 @@ const routes: Routes = [
     children: [{
       path: 'categorias',
       loadChildren: () => import('../categorias/categorias.module').then(m => m.CategoriasModule),
-      pathMatch: 'full'
+      pathMatch: 'full',
+      data: {
+        titulo: "Categoria",
+        subTitulo: "Realize o cadastro de novas categorias."
+      }
     },
     {
       path: 'lugares',
       loadChildren: () => import('../lugares/lugares.module').then(m => m.LugaresModule),
-      pathMatch: 'full'
+      pathMatch: 'full',
+      data: {
+        titulo: "Lugares",
+        subTitulo: "Realize o cadastro de novos lugares"
+      }
     },
     {
       path: 'galeria',
       loadChildren: () => import('../galeria/galeria.module').then(m => m.GaleriaModule),
-      pathMatch: 'full'
+      pathMatch: 'full',
+      data: {
+        titulo: "Lista de lugares legais",
+        subTitulo: "Descubra a realização do seu próximo sonho."
+      }
     },
     ]
   }
